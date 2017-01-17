56a9821c72f94438faa5f6f0dbfe502ee1e956cc
Tymon_Solecki <tymon.solecki@gmail.com>
2017-01-17 16:01:51 +0000

    find the line seems to work. Sometimes. Maybe.

--- assignment3.java
+++ assignment3.java
@@ -15,11 +15,44 @@ public class assignment3 {
     /**
      * @param args the command line arguments
      */
-    //robot goes forward until it finds a black line
-    /*public static findTheLine() {
+    /*
+      robot goes forward until it finds a black line
+      it goes straightforward until black  or brown is seen - then it rotate to the left until it sees the line again.
+    */
+    public static void findTheLine(Robot myRobot, Motor leftMotor, Motor rightMotor, Speaker speaker, ColorSensor colorSensor) {
 
-    }*/
-    //robot follows the black line (ideally to the left) until it finds a coloured dot
+        ColorSensor.Color x;
+        leftMotor.forward();
+        rightMotor.forward();
+        x = colorSensor.getColor();
+        while(x != ColorSensor.Color.BLACK && x!=ColorSensor.Color.BROWN) {
+          System.out.println(x);
+          leftMotor.stop();
+          rightMotor.stop();
+            leftMotor.setSpeed(100);
+            rightMotor.setSpeed(100);
+            leftMotor.forward();
+            rightMotor.forward();
+            myRobot.sleep(100);
+
+            x = colorSensor.getColor();
+        }
+        leftMotor.setSpeed(0);
+        rightMotor.setSpeed(80);
+        do {
+          x = colorSensor.getColor();
+        }
+        while(x!=ColorSensor.Color.BLACK);
+
+        return;
+
+    }
+
+    /*
+      robot follows the black line (ideally to the left) until it finds a coloured dot
+      it goes by the right edge of the dot.\
+      TODO: add next behaviour after the dot is found.
+    */
     public static void followTheLine(Robot myRobot, Motor leftMotor, Motor rightMotor, Speaker speaker, ColorSensor colorSensor) {
         ColorSensor.Color x;
         int i =0;
@@ -81,8 +114,8 @@ public class assignment3 {
 
         Speaker speaker = myRobot.getSpeaker();
         //testFunctions(myRobot, leftMotor, rightMotor, speaker, colorSensor);
+        findTheLine(myRobot, leftMotor, rightMotor, speaker, colorSensor);
         followTheLine(myRobot, leftMotor, rightMotor, speaker, colorSensor);
-
 
         myRobot.close();
 
